badd2d167df6eaf0fb6c3b8a0cfd220e8c973270
Carlos Sanchez <admroser@gmail.com>
2018-09-10 21:30:52 -0400

    Main Content and footer need to be fixed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // document.getElementbyClassName
+
+// const navbar = document.getElementsByClassName('topnav');
+const whatWeOfferlist = document.getElementById('what-we-offer');
+// const footer = document.getElementsByClassName('footer');
+
+offerList = [
+    'Premiun Materials',
+    'Fast Shipping',
+    'Quality Insurance'
+]
+
+offerListContent = ["Our robot is made with aluminiun, which will increase duration of our product",
+    "We make sure your robot is ready to shipped as soon as it is ordered.",
+    "No damages or faults"
+]	
+
+footerContent=[
+	'Privacy',
+	'Terms',
+	'Contact'
+]
+
+function navbarEffect() {
+    navbar.classList.add('sticky');
+}
+var printThis = ""
+for (let i = 0; i < offerList.length; i++) {
+    printThis += `<h3> ${offerList[i]}  </h3>`;
+    for (let j = 0; j < offerListContent.length; ++j) {
+        printThis += `<p> ${offerListContent[j]}</p>`;
+        whatWeOfferlist.innerHTML = printThis;
+    }
+}
+
+let footer = document.getElementsByClassName('footer');
+let newFooterUl=document.createElement('ul');
+footer.appendChild(newFooterUl);  
+// for(let k = 0; k < footerContent.length; k++) {
+
+// }
