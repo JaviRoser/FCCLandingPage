b3bad0e6822c6707fc8e2a34a0cb34a37892fddd
Carlos Sanchez <admroser@gmail.com>
2018-09-14 21:06:38 -0400

    Add Video and pass all the tests

--- js/index.js
+++ js/index.js
@@ -1,40 +1,52 @@
 // const navbar = document.getElementsByClassName('topnav');
-const whatWeOfferlist = document.getElementById('what-we-offer');
-let footer = document.querySelector('footer');
+const whatWeOfferlist = document.getElementById("what-we-offer");
+let footer = document.querySelector("footer");
 
-offerList = [
-    'Premiun Materials',
-    'Fast Shipping',
-    'Quality Insurance'
-]
+let offerList = {
+    features: [{
+            imgSrc: "fas fa-thumbs-up",
+            title: "Premium Materials",
+            content: "Our robot is made with aluminiun, which will increase duration of our product"
+        },
+        {
+            imgSrc: "fas fa-shipping-fast",
+            title: "Fast Shipping",
+            content: "We make sure your robot is ready to shipped as soon as it is ordered."
+        },
+        {
+            imgSrc: "fas fa-certificate",
+            title: "Quality Insurance",
+            content: "No damages or faults"
+        }
+    ]
+};
 
-offerListContent = ["Our robot is made with aluminiun, which will increase duration of our product",
-    "We make sure your robot is ready to shipped as soon as it is ordered.",
-    "No damages or faults"
-]
+footerContent = ["Privacy", "Terms", "Contact"];
 
-footerContent = [
-    'Privacy',
-    'Terms',
-    'Contact'
-]
+// function navbarEffect() {
+//     navbar.classList.add("sticky");
+// }
 
-function navbarEffect() {
-    navbar.classList.add('sticky');
+/*Adds dynamically main content*/
+var printThis = "";
+for (let i = 0; i < offerList.features.length; i++) {
+    printThis += `<div class="offerContent">
+                    <div class="mainContentListIcons">
+                        <i class="${offerList.features[i].imgSrc}" aria-hidden="true"></i>
+                    </div>       
+                    <div class="offerDescription">
+                        <h3>${offerList.features[i].title}</h3>
+                        <p> ${offerList.features[i].content}</p>
+                    </div>
+
+                  </div>`;
+    whatWeOfferlist.innerHTML = printThis;
 }
-var printThis = ""
-for (let i = 0; i < offerList.length; i++) {
-    printThis += `<h3> ${offerList[i]}  </h3>`;
-    for (let j = 0; j < offerListContent.length; ++j) {
-        printThis += `<p> ${offerListContent[j]}</p>`;
-        whatWeOfferlist.innerHTML = printThis;
-    }
-}
 
-const copyrights=document.querySelector('.copyRights');
 //It dynamically adds the content to the footer content
+const copyrights = document.querySelector(".copyRights");
 for (let k = 0; k < footerContent.length; k++) {
-    const footerListContent = document.querySelector('.footerListContent')
+    const footerListContent = document.querySelector(".footerListContent");
     footerListContent.innerHTML += `<li>${footerContent[k]}</li>`;
 }
-copyrights.innerText= "Copyright 2016, Ninja Toy";
+copyrights.innerText = "Copyright 2016, Ninja Toy";
