7c7ee6c6c99cb4cf3716534a48d252a4fc77d237
Carlos Sanchez <admroser@gmail.com>
2018-09-19 10:35:27 -0400

    Fix the navbar size for responsive mode

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ let footer = document.querySelector("footer");
 
 let offerList = {
     features: [{
-            imgSrc: "fas fa-thumbs-up",
-            title: "Premium Materials",
-            content: "Our robot is made with aluminiun, which will increase duration of our product"
+            imgSrc:     "fas fa-thumbs-up",
+            title:      "Premium Materials",
+            content:    "Our robot is made with aluminiun, which will increase duration of our product"
         },
         {
-            imgSrc: "fas fa-shipping-fast",
-            title: "Fast Shipping",
-            content: "We make sure your robot is ready to shipped as soon as it is ordered."
+            imgSrc:     "fas fa-shipping-fast",
+            title:      "Fast Shipping",
+            content:    "We make sure your robot is ready to shipped as soon as it is ordered."
         },
         {
             imgSrc: "fas fa-certificate",
@@ -31,9 +31,9 @@ footerContent = ["Privacy", "Terms", "Contact"];
 var printThis = "";
 for (let i = 0; i < offerList.features.length; i++) {
     printThis += `<div class="offerContent">
-                    <div class="mainContentListIcons">
+                  <div class="mainContentListIcons">
                         <i class="${offerList.features[i].imgSrc}" aria-hidden="true"></i>
-                    </div>       
+                    </div>        
                     <div class="offerDescription">
                         <h3>${offerList.features[i].title}</h3>
                         <p> ${offerList.features[i].content}</p>
